be14dbabdadd74664d2bdbda00b6924fa3624f70
Vai69 <disalevaishnavi2001@gmail.com>
2022-09-28 11:58:52 +0530

    revised dependancies

--- screens/DamScreen.js
+++ screens/DamScreen.js
@@ -1,13 +1,20 @@
-import { StyleSheet , View , Text } from "react-native"
-
+import { StyleSheet, View, Text, TouchableOpacity, Image } from "react-native"
+import React, { useState, useEffect, useRef } from 'react'
+import CardFlip from 'react-native-card-flip';
 
 
 export default function DamScreen() {
+  const cardFlip = useRef();
   return (
-    <View>
-        <Text>
-            DamScreen
-        </Text>
+    <View style={styles.container}>
+      <CardFlip style={styles.cardContainer} ref={cardFlip}>
+        <TouchableOpacity style={styles.card} onPress={() => cardFlip.current.flip()}>
+          <Text>AVhgjghy</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.card} onPress={() => cardFlip.current.flip()}>
+          <Text>AVwjhsds</Text>
+        </TouchableOpacity>
+      </CardFlip>
     </View>
   )
 }
@@ -15,5 +22,8 @@ export default function DamScreen() {
 
 
 const styles = StyleSheet.create({
-
+  container: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
 })
